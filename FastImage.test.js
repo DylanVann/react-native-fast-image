2ec252446da6b8022348e6ed30c7025043e9b3dc
Dylan Vann <dylan.vann@gmail.com>
2017-04-17 19:50:22 -0400

    📸 Add basic snapshot test.

---
+++ FastImage.test.js
@@ -0,0 +1,20 @@
+import 'react-native'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import FastImage from './FastImage.js'
+
+test('FastImage renders correctly.', () => {
+  const tree = renderer.create(
+    <FastImage
+      source={{
+        uri: 'https://facebook.github.io/react/img/logo_og.png',
+        headers: {
+          token: 'someToken',
+        },
+        priority: FastImage.priority.high,
+      }}
+    />
+  ).toJSON()
+
+  expect(tree).toMatchSnapshot()
+})
