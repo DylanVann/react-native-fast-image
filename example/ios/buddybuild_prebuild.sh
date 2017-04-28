cc42a43f43a3c513a8d3744b01ccbd17e6c27035
Dylan Vann <dylan.vann@gmail.com>
2017-04-28 10:45:26 -0400

    Remove BuddyBuild configuration.

--- example/ios/buddybuild_prebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Workaround for a yarn bug: https://github.com/yarnpkg/yarn/issues/685
-echo "Removing duplicate node_modules, workaround for yarn bug."
-rm -rf ../node_modules/react-native-fast-image/node_modules
