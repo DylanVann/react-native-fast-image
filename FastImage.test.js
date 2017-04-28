1f0acfccf63c940874f6c5e003e217597630fa36
Dylan Vann <dylan.vann@gmail.com>
2017-04-28 11:31:58 -0400

    Run prettier.

--- FastImage.test.js
+++ FastImage.test.js
@@ -4,17 +4,19 @@ import renderer from 'react-test-renderer'
 import FastImage from './FastImage.js'
 
 test('FastImage renders correctly.', () => {
-  const tree = renderer.create(
-    <FastImage
-      source={{
-        uri: 'https://facebook.github.io/react/img/logo_og.png',
-        headers: {
-          token: 'someToken',
-        },
-        priority: FastImage.priority.high,
-      }}
-    />
-  ).toJSON()
+  const tree = renderer
+    .create(
+      <FastImage
+        source={{
+          uri: 'https://facebook.github.io/react/img/logo_og.png',
+          headers: {
+            token: 'someToken',
+          },
+          priority: FastImage.priority.high,
+        }}
+      />,
+    )
+    .toJSON()
 
   expect(tree).toMatchSnapshot()
 })
