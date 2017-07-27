aa6c1836e746adfbcdf9cab06c81ae0d1447f565
Dylan Vann <dylan.vann@gmail.com>
2017-07-26 21:57:15 -0400

    Fix android progress events.

--- android/src/main/java/com/dylanvann/fastimage/ProgressListener.java
+++ android/src/main/java/com/dylanvann/fastimage/ProgressListener.java
@@ -1,8 +1,8 @@
 package com.dylanvann.fastimage;
 
-public interface UIProgressListener {
+public interface ProgressListener {
 
-    void onProgress(long bytesRead, long expectedLength);
+    void onProgress(String key, long bytesRead, long expectedLength);
 
     /**
      * Control how often the listener needs an update. 0% and 100% will always be dispatched.
