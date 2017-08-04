367024bcbdd8571cab7c90e064b1b59cc21ef3a7
Aaron Taylor <me@exphat.com>
2017-08-04 17:05:21 -0400

    Remove override of createJSModules
    Since of recently in master the unused createJSModules has been removed and ReactPackage's no longer contain the definition of createJSModules. There for it must be removed in order to not yield a compile error. This is breaking change.
    
    facebook/react-native@ce6fb33

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewPackage.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewPackage.java
@@ -18,11 +18,6 @@ public class FastImageViewPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.<ViewManager>singletonList(new FastImageViewManager());
     }
