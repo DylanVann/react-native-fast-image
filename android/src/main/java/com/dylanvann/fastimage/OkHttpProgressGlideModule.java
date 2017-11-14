66c0e16795a29f36dd566352a19b7d90e697df7b
Kingsley Ochu <ochu.kc@gmail.com>
2017-11-14 12:27:40 +0100

    Clear up unused imports

--- android/src/main/java/com/dylanvann/fastimage/OkHttpProgressGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/OkHttpProgressGlideModule.java
@@ -5,7 +5,6 @@ import android.os.Handler;
 import android.os.Looper;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
