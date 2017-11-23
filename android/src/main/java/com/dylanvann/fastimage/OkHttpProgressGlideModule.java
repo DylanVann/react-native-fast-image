41a3e940b93d667644627f39f569ee7ab3ea6b75
tychota <tychota@users.noreply.github.com>
2017-11-23 12:09:59 +0100

    Revert "Clear up unused imports"
    This reverts commit c552e1beac5680cf3bd95c4d554ad2d58f4c8d09.

--- android/src/main/java/com/dylanvann/fastimage/OkHttpProgressGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/OkHttpProgressGlideModule.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.os.Looper;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
