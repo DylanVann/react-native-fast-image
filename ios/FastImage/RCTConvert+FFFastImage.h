f7217c20c04400c8ac8174e9806b79569176d42a
GuoYufu <guoyufu@picclife.cn>
2017-12-15 22:32:16 +0800

    reset

--- ios/FastImage/RCTConvert+FFFastImage.h
+++ ios/FastImage/RCTConvert+FFFastImage.h
@@ -1,10 +1,4 @@
-#if __has_include(<React/RCTConvert.h>)
 #import <React/RCTConvert.h>
-#elif __has_include("React/RCTConvert.h")
-#import "React/RCTConvert.h"
-#else
-#import "RCTConvert.h"
-#endif
 
 @class FFFastImageSource;
 
