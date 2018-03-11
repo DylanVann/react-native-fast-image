04865c210a133e40542d6b589968cfd44d611629
Dylan Vann <dylan.vann@gmail.com>
2018-03-11 19:19:39 -0400

    Fix tests. Update dependencies and format command.

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native'
 import React from 'react'
 import renderer from 'react-test-renderer'
-import FastImage from './FastImage.js'
+import FastImage from './index.js'
 
 const style = StyleSheet.create({ image: { width: 44, height: 44 } })
 
@@ -28,7 +28,7 @@ test('Renders a normal Image when not passed a uri.', () => {
   const tree = renderer
     .create(
       <FastImage
-        source={require('./server/pictures/jellyfish.gif')}
+        source={require('./react-native-fast-image-example-server/pictures/jellyfish.gif')}
         style={style.image}
       />,
     )
