01d52e43637a4f12abe72ff4d1e9ff4b2ee2121a
Dylan Vann <dylan@dylanvann.com>
2018-05-24 12:24:47 -0400

    More refactoring.

--- react-native-fast-image-example/metro.config.js
+++ react-native-fast-image-example/metro.config.js
@@ -1,17 +1,16 @@
-const path = require('path');
-const blacklist = require('metro/src/blacklist');
+const path = require('path')
+const blacklist = require('metro/src/blacklist')
 
 module.exports = {
-    extraNodeModules: {
-    
-    },
-    getBlacklistRE: () => blacklist([
-        /[/\\]Users[/\\]dylan[/\\]repos[/\\]react-native-fast-image[/\\]node_modules[/\\]react-native[/\\].*/
-    ]),
+    extraNodeModules: {},
+    getBlacklistRE: () =>
+        blacklist([
+            /[/\\]Users[/\\]dylan[/\\]repos[/\\]react-native-fast-image[/\\]node_modules[/\\]react-native[/\\].*/,
+        ]),
     getProjectRoots: () => [
         // Include current package as project root
         path.resolve(__dirname),
         // Include symlinked packages as project roots
-        path.resolve('/Users/dylan/repos/react-native-fast-image')
+        path.resolve('/Users/dylan/repos/react-native-fast-image'),
     ],
-};
+}
