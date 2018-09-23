e68fe53fbeb06bd59670d2ec9faed9fd6215561a
Dylan Vann <dylan@dylanvann.com>
2018-09-23 18:24:06 -0400

    Replace with clean examples.

--- react-native-fast-image-example/android/app/src/main/java/com/reactnativefastimageexample/MainApplication.java
+++ react-native-fast-image-example/android/app/src/main/java/com/reactnativefastimageexample/MainApplication.java
@@ -3,9 +3,6 @@ package com.reactnativefastimageexample;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.dylanvann.fastimage.FastImageViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,9 +23,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage()
-          ,new ImagePickerPackage()
-          ,new VectorIconsPackage()
-          ,new FastImageViewPackage()
       );
     }
 
