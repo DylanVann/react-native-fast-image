75a6ce766c7fc2c1dd98a4e8d8c52c3aeeb3d506
(◕ᴥ◕) <ratson@users.noreply.github.com>
2018-09-24 05:54:24 +0800

    Handle null view.glideUrl.

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewManager.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewManager.java
@@ -101,6 +101,10 @@ class FastImageViewManager extends SimpleViewManager<FastImageViewWithUrl> imple
     public void onDropViewInstance(FastImageViewWithUrl view) {
         // This will cancel existing requests.
         requestManager.clear(view);
+        if (view.glideUrl == null) {
+            super.onDropViewInstance(view);
+            return;
+        }
         final String key = view.glideUrl.toString();
         FastImageOkHttpProgressGlideModule.forget(key);
         List<FastImageViewWithUrl> viewsForKey = VIEWS_FOR_URLS.get(key);
