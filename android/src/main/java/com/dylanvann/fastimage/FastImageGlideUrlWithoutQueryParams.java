381f56eda944e17f81032d4641785af1c4ea8c45
Kamil Badyla <kamilbadyla@gmail.com>
2019-03-11 12:44:18 +0100

    Build staticURL using java URI

--- android/src/main/java/com/dylanvann/fastimage/FastImageGlideUrlWithoutQueryParams.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageGlideUrlWithoutQueryParams.java
@@ -2,15 +2,29 @@ package com.dylanvann.fastimage;
 
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.load.model.Headers;
+import java.net.URISyntaxException;
 
 import java.net.URI;
 
 public class FastImageGlideUrlWithoutQueryParams extends GlideUrl {
     private String staticURL;
 
+    private String getUrlWithoutParameters(String url) throws URISyntaxException {
+        URI uri = new URI(url);
+        return new URI(uri.getScheme(),
+            uri.getAuthority(),
+            uri.getPath(),
+            null,
+            uri.getFragment()).toString();
+    }
+
     public FastImageGlideUrlWithoutQueryParams(String url, Headers headers) {
         super(url, headers);
-        staticURL = url.substring(0, url.lastIndexOf('?'));
+        try {
+            staticURL = this.getUrlWithoutParameters(url);
+        } catch (Exception e) {
+            staticURL = url.toString();
+        }
     }
 
     @Override
