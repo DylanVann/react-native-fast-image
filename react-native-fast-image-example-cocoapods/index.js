5e2b21b057174e726ed365dde5d48763547b0cdf
Dylan Vann <dylan@dylanvann.com>
2019-04-22 22:48:10 -0400

    feat: Upgrade example apps.

--- react-native-fast-image-example-cocoapods/index.js
+++ react-native-fast-image-example-cocoapods/index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import { AppRegistry } from 'react-native'
-import App from './src'
 import { name as appName } from './app.json'
+import App from './src'
 
 AppRegistry.registerComponent(appName, () => App)
