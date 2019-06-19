9f2f33a89121b1d8d02693cc681bbc3e27c0db4b
Todd Berman <tberman@gmail.com>
2019-06-19 10:42:00 -0700

    Use the device's main screen scale

--- ios/FastImage/FFFastImageView.m
+++ ios/FastImage/FFFastImageView.m
@@ -68,7 +68,7 @@
 
 - (UIImage*)makeImage:(UIImage *)image withTint:(UIColor *)color {
     UIImage *newImage = [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
-    UIGraphicsBeginImageContextWithOptions(image.size, NO, newImage.scale);
+    UIGraphicsBeginImageContextWithOptions(image.size, NO, 0.0);
     [color set];
     [newImage drawInRect:CGRectMake(0, 0, image.size.width, newImage.size.height)];
     newImage = UIGraphicsGetImageFromCurrentImageContext();
