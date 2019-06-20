9c19124fdf2a4c11d261136557b6318712a8ff81
vok <admin@MacOnes-MacBook-Pro-2.local>
2019-06-20 13:54:36 +0700

    feat: Upgrade to React Native 0.60.0 / CocoaPods / Android X.
    BREAKING CHANGE: You should upgrade React Native. See https://facebook.github.io/react-native/blog/2019/07/03/version-60

--- react-native-fast-image-example/src/BulletText.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import FeatureText from './FeatureText'
-
-const BulletText = ({ text, children }) => (
-    <FeatureText text={`• ${text || children} •`} />
-)
-
-export default BulletText
