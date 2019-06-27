e5611f844d626b58d248cfb8d508b99dbbefdea3
Dmytro Portenko <troublesohard@gmail.com>
2019-06-27 14:35:24 +0300

    Update FFFastImageView.m
    fix for https://github.com/DylanVann/react-native-fast-image/issues/504

--- ios/FastImage/FFFastImageView.m
+++ ios/FastImage/FFFastImageView.m
@@ -29,7 +29,7 @@
 
 - (void)setOnFastImageLoadEnd:(RCTDirectEventBlock)onFastImageLoadEnd {
     _onFastImageLoadEnd = onFastImageLoadEnd;
-    if (self.hasCompleted) {
+    if (self.hasCompleted && _onFastImageLoadEnd != NULL) {
         _onFastImageLoadEnd(@{});
     }
 }
