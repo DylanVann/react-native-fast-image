1c85caa582f11392dee9250c0864f8b00f873aa5
vidsag <vidhyasagar.mohanraj@gmail.com>
2019-08-13 15:28:54 +0530

    [Sagar] changed timeou to 20 seconds

--- android/src/main/java/com/dylanvann/fastimage/FastImageOkHttpProgressGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageOkHttpProgressGlideModule.java
@@ -45,7 +45,7 @@ public class FastImageOkHttpProgressGlideModule extends LibraryGlideModule {
         OkHttpClient client = OkHttpClientProvider
                 .getOkHttpClient()
                 .newBuilder()
-                .callTimeout(30, TimeUnit.SECONDS)
+                .callTimeout(20, TimeUnit.SECONDS)
                 .addInterceptor(createInterceptor(progressListener))
                 .build();
         OkHttpUrlLoader.Factory factory = new OkHttpUrlLoader.Factory(client);
