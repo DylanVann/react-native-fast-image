426acd4fbca79b7e07d1b6ee591e36f2d5a61b9c
Daniel Cohen Gindi <danielgindi@gmail.com>
2019-08-29 20:01:16 +0300

    Support tintColor from style like in RCTImageView

--- src/index.tsx
+++ src/index.tsx
@@ -211,6 +211,10 @@ function FastImageBase({
         }
     }
 
+    if ((tintColor === null || tintColor === undefined) && style) {
+        tintColor = StyleSheet.flatten(style).tintColor
+    }
+
     return (
         <View style={[styles.imageContainer, style]} ref={forwardedRef}>
             <FastImageView
