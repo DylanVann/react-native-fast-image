ac117060ebdd04b2130bfb23c62f203beb077089
Dylan Vann <dylan@dylanvann.com>
2020-03-08 20:44:22 -0400

    feat: converts to TypeScript (#642)
    This converts the codebase to TypeScript. Compilation and bundling is done with tsdx. This will
    hopefully ensure our types are more accurate in the future.

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native'
 import React from 'react'
 import renderer from 'react-test-renderer'
-import FastImage from './index.js'
+import FastImage from './index'
 
 const style = StyleSheet.create({ image: { width: 44, height: 44 } })
 
