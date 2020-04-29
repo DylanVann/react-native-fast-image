3e7900054e8b6247a80bd8d2b357d89700b5b8d6
Krisztiaan UIC <60505575+krisztiaan-uic@users.noreply.github.com>
2020-04-29 21:18:53 +0200

    fix(types): add derived types for priority and cache

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,7 @@ const resizeMode = {
     center: 'center',
 } as const
 
-type Priority = 'low' | 'normal' | 'high'
+type Priority = typeof priority[keyof typeof priority]
 
 const priority = {
     low: 'low',
@@ -31,7 +31,7 @@ const priority = {
     high: 'high',
 } as const
 
-type Cache = 'low' | 'normal' | 'high'
+type Cache = typeof cacheControl[keyof typeof cacheControl]
 
 const cacheControl = {
     // Ignore headers, use uri as cache key, fetch only if not in cache.
