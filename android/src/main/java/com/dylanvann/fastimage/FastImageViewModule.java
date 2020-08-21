6d8570623ad2219cdd24b5ff16bfefd639fd1a4a
Alexandru Mic <alexandru.mic@around25.com>
2020-08-21 10:33:24 +0300

    add getCachePath for Android

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewModule.java
@@ -2,8 +2,14 @@ package com.dylanvann.fastimage;
 
 import android.app.Activity;
 
+import android.support.annotation.Nullable;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -11,9 +17,12 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.views.imagehelper.ImageSource;
 
+import java.io.File;
+
 class FastImageViewModule extends ReactContextBaseJavaModule {
 
     private static final String REACT_CLASS = "FastImageView";
+    private static final String ERROR_LOAD_FAILED = "ERROR_LOAD_FAILED";
 
     FastImageViewModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -50,6 +59,40 @@ class FastImageViewModule extends ReactContextBaseJavaModule {
                             .apply(FastImageViewConverter.getOptions(activity, imageSource, source))
                             .preload();
                 }
+            }
+        });
+    }
+
+    @ReactMethod
+    public void getCachePath(final ReadableMap source, final Promise promise) {
+        final Activity activity = getCurrentActivity();
+        if (activity == null) return;
+
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                final FastImageSource imageSource = FastImageViewConverter.getImageSource(activity, source);
+                final GlideUrl glideUrl = imageSource.getGlideUrl();
+
+                Glide
+                        .with(activity.getApplicationContext())
+                        .asFile()
+                        .load(glideUrl)
+                        .apply(FastImageViewConverter.getOptions(activity.getApplicationContext(), imageSource,  source))
+                        .listener(new RequestListener<File>() {
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<File> target, boolean isFirstResource) {
+                                promise.reject(ERROR_LOAD_FAILED, e);
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(File resource, Object model, Target<File> target, DataSource dataSource, boolean isFirstResource) {
+                                promise.resolve(resource.getAbsolutePath());
+                                return false;
+                            }
+                        })
+                        .submit();
             }
         });
     }
