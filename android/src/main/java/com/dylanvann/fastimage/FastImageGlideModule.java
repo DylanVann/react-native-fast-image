fc6359f487abc71e2cf0c5659068bb16db49082a
Hanno Gödecke <hgoedecke@cuvent.com>
2021-03-23 12:08:58 +0100

    improve(android): increased glide cache to 1GB

--- android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageGlideModule.java
@@ -10,6 +10,7 @@ import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
 import com.dylanvann.fastimage.custom.SharedOkHttpClient;
@@ -19,6 +20,8 @@ import java.io.InputStream;
 // We need an AppGlideModule to be present for progress events to work.
 @GlideModule
 public final class FastImageGlideModule extends AppGlideModule {
+    final long ONE_GB = 1024L * 1024L * 1000L; // 1000 MB
+
     //this overwrites the default okhttp client with a client that has caching enabled.
     @Override
     public void registerComponents(Context context, Glide glide, Registry registry) {
@@ -31,5 +34,6 @@ public final class FastImageGlideModule extends AppGlideModule {
     public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
         super.applyOptions(context, builder);
         builder.setLogLevel(Log.VERBOSE);
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, ONE_GB));
     }
 }
