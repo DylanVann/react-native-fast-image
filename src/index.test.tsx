7dcb4aeba0da92978979ad814a7a1c44ce53d871
Mickagd <ymichael.girier-dufournier@epitech.eu>
2021-08-18 14:01:26 +0200

    fix NativeEventEmitter mock in test

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,4 +1,4 @@
-import { NativeEventEmitter, StyleSheet } from 'react-native'
+import { StyleSheet } from 'react-native'
 import React from 'react'
 import renderer from 'react-test-renderer'
 import FastImage from './index'
