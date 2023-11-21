c2e6a76b666a00c54f080f85e0bf3866e71b36b3
Christoph Pader <christoph@margelo.io>
2023-11-21 16:07:55 +0100

    remove import

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewWithUrl.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewWithUrl.java
@@ -2,7 +2,6 @@ package com.dylanvann.fastimage;
 
 import static com.dylanvann.fastimage.FastImageRequestListener.REACT_ON_ERROR_EVENT;
 
-import androidx.annotation.NonNull;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
