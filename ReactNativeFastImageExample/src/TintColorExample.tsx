434a41338122326bb9c806cfc68ef524542486ea
Dylan Vann <dylan@dylanvann.com>
2026-09-24 13:53:25 +0700

    chore(example): add remote images to the tint example
    Remote images load through SDWebImage and have no defaultSource, which is the path that crashed before #1082.

--- ReactNativeFastImageExample/src/TintColorExample.tsx
+++ ReactNativeFastImageExample/src/TintColorExample.tsx
@@ -8,6 +8,9 @@ import FeatureText from './FeatureText'
 // @ts-ignore
 import LogoImage from './images/logo.png'
 
+const REMOTE_LOGO =
+    'https://raw.githubusercontent.com/DylanVann/react-native-fast-image/main/ReactNativeFastImageExample/src/images/logo.png'
+
 export const TintColorExample = () => {
     return (
         <View>
@@ -30,6 +33,29 @@ export const TintColorExample = () => {
                     style={styles.image}
                     tintColor={'rgba(0,0,0,0.5)'}
                     source={LogoImage}
+                />
+            </SectionFlex>
+            <Section>
+                <FeatureText text="Remote images with tint color (no defaultSource)." />
+            </Section>
+            <SectionFlex>
+                <FastImage
+                    style={styles.image}
+                    tintColor={'green'}
+                    source={{ uri: REMOTE_LOGO }}
+                    resizeMode="contain"
+                />
+                <FastImage
+                    style={styles.image}
+                    tintColor={'#9324c3'}
+                    source={{ uri: REMOTE_LOGO }}
+                    resizeMode="contain"
+                />
+                <FastImage
+                    style={styles.image}
+                    tintColor={'rgba(0,0,0,0.5)'}
+                    source={{ uri: REMOTE_LOGO }}
+                    resizeMode="contain"
                 />
             </SectionFlex>
         </View>
